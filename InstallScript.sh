f532fa3961b24accfdbe5e084911d0392f5665e9
tristan-caetano <tristancostacaetano@gmail.com>
2025-03-10 18:59:39 -0400

    Adding wget applications, and creating directories

--- InstallScript.sh
+++ InstallScript.sh
@@ -3,10 +3,15 @@
 DNF_INSTALL="dnf -y install"
 FLAT_INSTALL="flatpak install -y"
 
+SCRIPTS_DIR=$(pwd)
+APPLICATIONS_DIR="/home/$USER/Documents/Applications"
+
+mkdir $APPLICATIONS_DIR
+
 # RPM Fusion
 $DNF_INSTALL https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 $DNF_INSTALL https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-dnf update
+dnf -y update
 
 # VSCode
 $DNF_INSTALL code
@@ -28,6 +33,9 @@ $DNF_INSTALL vim
 # Steam
 $DNF_INSTALL steam
 
+# VLC Media Player
+$DNF_INSTALL vlc
+
 # Polychromatic (for Razer Stuff)
 dnf config-manager addrepo --from-repofile=https://openrazer.github.io/hardware:razer.repo
 $DNF_INSTALL polychromatic
@@ -38,17 +46,45 @@ $DNF_INSTALL akmod-nvidia
 $DNF_INSTALL xorg-x11-drv-nvidia-cuda
 $DNF_INSTALL xorg-x11-drv-nvidia-libs
 
+# hide.me install
+cd $APPLICATIONS_DIR
+mkdir hide.me
+cd hide.me
+curl -L https://hide.me/download/linux-amd64 | tar -xz && sudo ./install.sh
+
+# Minecraft download
+cd $APPLICATIONS_DIR
+wget https://launcher.mojang.com/download/Minecraft.tar.gz
+tar -xvf Minecraft.tar.gz
+rm Minecraft.tar.gz
+
+# Runelite download
+cd $APPLICATIONS_DIR
+mkdir RuneLite
+cd RuneLite
+wget https://github.com/runelite/launcher/releases/download/2.7.3/RuneLite.AppImage
+chmod +x RuneLite.AppImage
+
+# Runelite download
+cd $APPLICATIONS_DIR
+mkdir ArduinoIDE
+cd ArduinoIDE
+wget https://downloads.arduino.cc/arduino-ide/nightly/arduino-ide_nightly-latest_Linux_64bit.AppImage
+chmod +x arduino-ide_nightly-latest_Linux_64bit.AppImage
+
 # Sourcing script in .bashrc
-echo source /home/tcaetano/Documents/Scripts/MyBashrc.sh >> /home/tcaetano/.bashrc
+echo source $SCRIPTS_DIR/MyBashrc.sh >> /home/tcaetano/.bashrc
 
 # Chmod stuff
+cd $SCRIPTS_DIR
 chmod +x MyBashrc.sh
 chmod +x StartupScript.sh
 
 # Downloaded RPMS
-#$DNF_INSTALL /home/tcaetano/Downloads/goxlr-utility-*.x86_64.rpm
-#$DNF_INSTALL /home/tcaetano/Downloads/VNC-Viewer-*-Linux-x64.rpm
-#$DNF_INSTALL /home/tcaetano/Downloads/powershell-*.rh.x86_64.rpm
+# The version numbers in the url prevent these from being automatically downloaded without webscraping
+#$DNF_INSTALL /home/$USER/Downloads/goxlr-utility-*.x86_64.rpm
+#$DNF_INSTALL /home/$USER/Downloads/VNC-Viewer-*-Linux-x64.rpm
+#$DNF_INSTALL /home/$USER/Downloads/powershell-*.rh.x86_64.rpm
 
 # Reboot
 reboot -nf
